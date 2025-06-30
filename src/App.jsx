04dfe9f98ab76038505b9ec18e0e3166bb21eac7
kunal <rohna1236@gmail.com>
2025-07-01 00:35:33 +0530

    made a structure for forms using tailwind

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+import './App.css';
+import Forms from './components/forms';
+
+function App(){
+  return (
+    < div className='container mx-auto'>
+     <Forms/>
+    </div>
+  )
+}
+
+export default App;
---
+++ src/components/forms/index.jsx
@@ -0,0 +1,17 @@
+const Forms = () => {
+  return (
+    <div className="flex flex-row items-center justify-center  h-screen gap-45  ">
+      {/* Create Room box */}
+      <div className=" w-80 h-80 border-1 border-blue-500 flex justify-center pt-5 ">
+        <h1 className="text-blue-600 font-bold text-3xl">Create Room</h1>
+      </div>
+
+      {/* Join Room box */}
+      <div className="w-80 h-80 border-1 border-blue-500 flex justify-center pt-5 ">
+        <h1 className="text-blue-600 font-bold text-3xl">Join Room</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Forms;
